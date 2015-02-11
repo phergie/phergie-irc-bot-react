b3e955876f6e8e4ef3565142afcacc3c4bd90c57
hashworks <admin@hashworks.net>
2015-02-11 18:29:08 +0100

    Update copyright to 2015

--- src/PluginProcessor/ClientInjector.php
+++ src/PluginProcessor/ClientInjector.php
@@ -3,7 +3,7 @@
  * Phergie (http://phergie.org)
  *
  * @link http://github.com/phergie/phergie-irc-bot-react for the canonical source repository
- * @copyright Copyright (c) 2008-2014 Phergie Development Team (http://phergie.org)
+ * @copyright Copyright (c) 2008-2015 Phergie Development Team (http://phergie.org)
  * @license http://phergie.org/license New BSD License
  * @package Phergie\Irc\Bot\React
  */
